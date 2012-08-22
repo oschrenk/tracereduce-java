6bee9353f7c2bb6c8e88936f5a6bf8e5d27b6354
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-22 11:56:08 +0200

    Initial commit linear optimum opencl

---
+++ primitive/double/algorithms/linear-approximation/opencl/src/main/java/de/q2web/gis/alg/la/opencl/LinearApproximationOpenClAlgorithm.java
@@ -0,0 +1,16 @@
+package de.q2web.gis.alg.la.opencl;
+
+import java.util.List;
+
+import de.q2web.gis.trajectory.core.api.Algorithm;
+import de.q2web.gis.trajectory.core.api.Point;
+
+public class LinearApproximationOpenClAlgorithm implements Algorithm {
+
+	@Override
+	public List<Point> run(List<Point> trace, double epsilon) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
